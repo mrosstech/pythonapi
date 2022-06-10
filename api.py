7df4022fc7ff8c764e3a8983e4e6f559bad6103a
Michael Ross <mrosstech@gmail.com>
2022-06-10 10:29:29 -0400

    fixed errors in get route

--- api.py
+++ api.py
@@ -1,18 +1,19 @@
 import os
 import glob
+import json
 import pandas as pd
 from flask import Flask, request
 app = Flask(__name__)
 
 # define file locations:
 data_dir = os.path.join(app.instance_path, 'data')
-os.makedirs(data_dir, exists_ok=True)
+os.makedirs(data_dir, exist_ok=True)
 
 @app.route('/datasets/', methods=['GET'])
 def getDatasets():
     # Get all files with the pkl extension in the data directory
     datasets = glob.glob(data_dir + "/*.pkl")
-    return datasets
+    return json.dumps(datasets)
 
 @app.route('/datasets/', methods=['POST'])
 def postDatasets():
@@ -20,6 +21,7 @@ def postDatasets():
     if csvFile:
         df = pd.read_csv(csvFile)
         df.to_pickle(os.path.join(data_dir, csvFile.filename + ".pkl"))
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 
 
